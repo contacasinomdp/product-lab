a699b09cf113490254b02b826b59d7ea7f042e56
jmmmdp <jmmmdp@gmail.com>
2026-02-06 21:30:49 -0300

    Week2: applied JavaScript logic

--- src/week-02/basics.js
+++ src/week-02/basics.js
@@ -23,5 +23,33 @@ if(edadUsuario >= edadMinima){
     console.log("Acceso Denegado");
 }
 
-const nombre="Juan";
-nombre="Pedro"; // Esto genera un error porque 'nombre' es una constante
+//Ejercicio 1   
+
+const precio=1000;
+let edadCliente=20;
+const IVA=0.10;
+let tieneDescuento=true;
+
+const edadMini=18;
+
+if (edadCliente<edadMini){
+    console.log("El cliente no tiene edad suficiente para comprar el producto");
+}
+else {
+    console.log("El cliente tiene edad suficiente para comprar el producto");
+
+}
+
+const montoIVA=precio*IVA;
+const precioFin=precio+montoIVA;  
+
+if (tieneDescuento){
+    const descuento=0.20;
+    const montoDescuento=precioFin*descuento;
+    const precioConDescuento=precioFin-montoDescuento;
+    
+}
+console.log("El precio base del producto es: ", precio);
+console.log("El monto de IVA es: ", montoIVA);
+console.log("El precio final del producto es: ", precioFin);
+    
